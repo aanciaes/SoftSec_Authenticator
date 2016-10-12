e21fdcc792c4915aa4592291d09df64ce3f43346
miguel <miguel@miguel-mint>
2016-10-12 16:53:03 +0100

    Added Account Interface

---
+++ src/softwaresecurity/authenticator/AccountInterface.java
@@ -0,0 +1,47 @@
+package softwaresecurity.authenticator;
+
+public interface AccountInterface {
+
+	/**
+	 * Changes current password to given password
+	 * @param pwd New Password
+	 */
+	void changePWD ( String pwd);
+	
+	/**
+	 * Change the lock state
+	 * @param locked_in lock state to be changed to
+	 */
+	void changeLocked (boolean locked_in);
+	
+	/**
+	 * Change current logged in state
+	 * @param logged_in logg state to be change to
+	 */
+	void changeLogin (boolean logged_in);
+	
+	/**
+	 * Return the account name
+	 * @return The account Name
+	 */
+	String getAccountName ();
+	
+	/**
+	 * Return the password
+	 * @return password
+	 */
+	String getPassword ();
+	
+	/**
+	 * Returns true if this account is currently looged in
+	 * @return true if this account is currently logged in
+	 */
+	boolean isLoggedIn ();
+	
+	
+	/**
+	 * Returns true if the account is currently locked
+	 * @return true if the account is currently locked
+	 */
+	boolean isLocked();
+}
